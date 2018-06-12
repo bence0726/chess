94f51f9501d65c0678c0bab4f1309a4ee55cc97f
VARGA <U588319@ads.dlh.de>
2018-06-12 02:16:20 +0200

    engage if possible

--- game.js
+++ game.js
@@ -18,8 +18,71 @@ var makeRandomMove = function() {
   if (possibleMoves.length === 0) return;
 
   var randomIndex = Math.floor(Math.random() * possibleMoves.length);
-  game.move(possibleMoves[randomIndex]);
+  return possibleMoves[randomIndex];
+};
+
+var calculateBestMove = function () {
+
+  var newGameMoves = game.moves();
+  console.log(newGameMoves);
+  var bestMove = null;
+  //use any negative large number
+  var bestValue = -9999;
+
+  for (var i = 0; i < newGameMoves.length; i++) {
+      var newGameMove = newGameMoves[i];
+      game.move(newGameMove);
+
+      //take the negative as AI plays as black
+      var boardValue = -evaluateBoard(game.board());
+      game.undo();
+      if (boardValue > bestValue) {
+          bestValue = boardValue;
+          bestMove = newGameMove;
+      }
+  }
+  if (bestValue === 0){
+    game.move(makeRandomMove());
+  }else{
+    game.move(bestMove);
+  }
   board.position(game.fen());
+
+};
+
+var evaluateBoard = function (board) {
+  var totalEvaluation = 0;
+  for (var i = 0; i < 8; i++) {
+      for (var j = 0; j < 8; j++) {
+          totalEvaluation = totalEvaluation + getPieceValue(board[i][j]);
+      }
+  }
+  return totalEvaluation;
+};
+
+var getPieceValue = function (piece) {
+  if (piece === null) {
+      return 0;
+  }
+  var getAbsoluteValue = function (piece) {
+      if (piece.type === 'p') {
+          return 10;
+      } else if (piece.type === 'r') {
+          return 50;
+      } else if (piece.type === 'n') {
+          return 30;
+      } else if (piece.type === 'b') {
+          return 30;
+      } else if (piece.type === 'q') {
+          return 90;
+      } else if (piece.type === 'k') {
+          return 900;
+      }
+      throw "Unknown piece type: " + piece.type;
+  };
+
+  var absoluteValue = getAbsoluteValue(piece);
+  return piece.color === 'w' ? absoluteValue : -absoluteValue;
 };
 
 var onDrop = function(source, target) {
@@ -34,7 +97,7 @@ var onDrop = function(source, target) {
   if (move === null) return 'snapback';
 
   // make random legal move for black
-  window.setTimeout(makeRandomMove, 250);
+  window.setTimeout(calculateBestMove, 250);
 };
 
 // update the board position after the piece snap
